20e2ec5a5fbf525cc9bd322d7ed68549b2395150
hannesi <hannesi@iki.fi>
2022-05-18 19:12:05 +0300

    Dialogue mechanics pretty much finished

--- tunturi/Assets/Scripts/dialogue/Dialogue.cs
+++ tunturi/Assets/Scripts/dialogue/Dialogue.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
-    public string[] participants;
+    public DialogueParticipant[] participants;
     public DialogueLine[] lines;
     public bool voiceEffects;
-    public Texture speakerAvatar;
 }
+
